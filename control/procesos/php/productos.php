c375aebb7e1e92e2d98352a1285b582f1cb392b6
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-05-08 14:01:17 -0300

    Update productos.php

--- control/procesos/php/productos.php
+++ control/procesos/php/productos.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 error_reporting(E_ALL);
-include_once "../../conexion/conexioni.php";
+include_once __DIR__ . "/../../../conexion/conexioni.php";
 
 header('Content-Type: application/json');
 
