fc13a58cfd2ac0dd89109673d954cc85e2c2ffdc
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-05-10 23:03:35 -0300

    session start

--- control/procesos/php/ventas.php
+++ control/procesos/php/ventas.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+session_start();
 include_once __DIR__ . "/../../../conexion/conexioni.php";
 
 header('Content-Type: application/json; charset=utf-8');
