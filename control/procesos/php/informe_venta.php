3def2832d73fc026eae8f6684c03100031e42bcc
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-05-27 15:23:37 -0300

    Update informe_venta.php

--- control/procesos/php/informe_venta.php
+++ control/procesos/php/informe_venta.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 session_start();
 include_once "../../../conexion/conexioni.php";
 date_default_timezone_set("America/Argentina/Cordoba");
